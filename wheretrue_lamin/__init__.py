5d88986f98e8e4dee9357216b72221440ca54f42
zethson <lukas.heumos@posteo.net>
2024-09-09 14:04:38 +0200

    :art: Docs
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- wheretrue_lamin/__init__.py
+++ wheretrue_lamin/__init__.py
@@ -9,8 +9,6 @@ This is the complete API reference:
 .. autosummary::
    :toctree: .
 
-   example_function
-   ExampleClass
 """
 
 __version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1rc1
