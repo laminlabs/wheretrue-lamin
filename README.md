653c566d098f7e2f732f1ae337fa2f3e8d8c68f5
zethson <lukas.heumos@posteo.net>
2024-09-09 13:49:23 +0200

    🎉 Initial commit
    Signed-off-by: zethson <lukas.heumos@posteo.net>

---
+++ README.md
@@ -0,0 +1,5 @@
+# wheretrue-lamin: Collaboration between wheretrue and lamin
+
+## Contributing
+
+Please run `pre-commit install` and `gitmoji -i` on the CLI before starting to work on this repository!
---
+++ wheretrue_lamin/__init__.py
@@ -0,0 +1,18 @@
+"""Collaboration between wheretrue and lamin.
+
+Import the package::
+
+   import wheretrue_lamin
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   example_function
+   ExampleClass
+"""
+
+__version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1rc1
+
+from ._core import ExampleClass, example_function
